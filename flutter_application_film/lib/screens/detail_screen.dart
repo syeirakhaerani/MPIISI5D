2e2d6aca89a1caff048d92d19e4cf2d5461014b2
syeirakhaerani <syeirakhaerani@gmail.com>
2025-03-12 09:13:32 +0700

    Update detail_screen.dart

--- flutter_application_film/lib/screens/detail_screen.dart
+++ flutter_application_film/lib/screens/detail_screen.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_film/models/movie.dart';
 
-class DetailScreen extends StatefulWidget {
-  const DetailScreen({super.key});
+class DetailScreen extends StatelessWidget {
+  final Movie movie;
+  const DetailScreen({super.key, required this.movie});
 
-  @override
-  State<DetailScreen> createState() => _DetailScreenState();
-}
-
-class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movie.title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            'https://image.tmdb.org/t/p/w500${movie.backdropPath}',
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          )
+          const SizedBox(
+            height: 10,
+          )
+          Text(movie.overview),
+          const SizedBox(
+            height: 20,
+          )
+          Row(
+            children: [
+              const Icon(Icons.calendar_month, color: Colors.blue,),
+              const SizedBox( width: 10,)
+            ],
+          )
+        ],
+      ));
   }
 }
